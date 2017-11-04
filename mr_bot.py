e81fdd7083eb89e4ec55f3649304f06678b42545
Mozzius <mozzius.iii@gmail.com>
2017-11-04 14:08:07 +0000

    100% better

--- mr_bot.py
+++ mr_bot.py
@@ -1,76 +1,73 @@
-import discord
-import asyncio
 import string
+import asyncio
+import discord
 
-class mr_bot:
-    def __init__(self):
-        self.conversing = False
-        self.context = []
-        self.joke
+bot = discord.Client()
 
-        global client
-        client = discord.Client()
-        with open("token.txt") as o:
-            client.run(o.readline())
+print("Initialising...")
 
-    def restring(self, stringle):
-        temp = ""
-        for word in stringle:
-            temp += word
-            temp += " "
-        return temp[:-1]
-    
-    def parse(self, message):
-        self.message = message
-        msg = message.content.translate(None, string.punctuation)
-        if msg.startswith('hey') or msg.startswith('yo') or msg.startswith('excuse me'):
-            split = msg.split(" ")
-            if (split[1] == "@mr" and split[2] == "bot#0848") or (split[1] == "mr" and split[2] == "bot"):
-                print(msg)
-                print(msg[3:])
-                if msg:
-                    self.interpret(msg[3:])
-                else:
-                    await client.send_message(message.channel, 'hey, what\'s up?')
-                    self.conversing = True
-        elif msg.startswith('mr bot') or msg.startswith('@mr bot'):
-            msg = msg.split(" ")
-            if msg:
-                self.interpret(msg[2:])
-            else:
-                await client.send_message(message.channel, 'what')
-        elif self.conversing == True:
-            if msg.startswith('thanks') or msg.startswith('thank you') or msg.startswith('thx'):
-                await client.send_message(message.channel, 'you\'re welcome :)')
-            else:
-                msg = msg.split(" ")
-                self.interpret(msg)
+def parse(msg):
+    if msg.startswith('hey') or msg.startswith('yo'):
+        splitmsg = msg.split(" ")
+        if (splitmsg[1] == "@mr" and splitmsg[2] == "bot#0848") or (splitmsg[1] == "mr" and splitmsg[2] == "bot"):
+            return 3
+    elif msg.startswith('mr bot') or msg.startswith('@mr bot#0848'):
+        return 2
+
+def parsecommand(commandarr):
+    if commandarr[0] == "textmute" or (commandarr[0] == "shut" and commandarr[1] == "up"):
+        return "mute"
+    if commandarr[0] == "introduce":
+        return "intro"
+    if commandarr[0] == "search" or (commandarr[0] == "look" and commandarr[1] == "up"):
+        return "mute"
+    if commandarr[0] == "is":
+        return "is"
+    else:
+        return "unsure"
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.event
+async def on_message(message):
+    msg = message.content
+    msg.strip(".,!?").lower()
+    relevent = parse(msg)
+    if relevent:
+        splitmsg = msg.split(" ")
+        if len(splitmsg) == relevent:
+            await bot.send_message(message.channel, "hey, what's up?")
         else:
-            conversing = False
+            command = ""
+            commandarr = splitmsg[relevent:]
+            #for word in commandarr:
+            #    command += word
+            #    command += " "
+            keyword = parsecommand(commandarr)
+            if keyword == "mute":
+                if str(message.author.top_role) == "moddo":
+                    await bot.send_message(message.channel, "Muting "+commandarr[1])
+                else:
+                    await bot.send_message(message.channel, "You don't have permission to do that, "+message.author.nick)
+            elif keyword == "intro":
+                if message.author.top_role == "moddo":
+                    await bot.send_message(message.channel, "Hey @everybody! I'm your new bot friend, mr bot. Summond me using \"hey mr bot\"!")
+                else:
+                    await bot.send_message(message.channel, "No")
+            elif keyword == "search":
+                await bot.send_message(message.channel, "I would, but I don't know how :(")
+            elif keyword == "is":
+                await bot.send_message(message.channel, "Probably")
+            elif keyword == "unsure":
+                await bot.send_message(message.channel, "Not sure what that means, sorry :/")
+                
 
-    def interpret(self, msg):
-        msg = restring(msg)
-        if "tell me a joke" in msg or "tell a joke" in msg or ("another one" in msg and "joke" in self.context and self.conversing == True):
-            await client.send_message(message.channel, '@Leprechaun#4633')
-            if "joke" not in self.context:
-                self.context = ["joke"]
-        elif self.conversing == True and "feedback" in self.context and "yes" in msg:
-            await client.send_message(message.channel, 'Oops! I\'ll take a look at it, thanks')
-            print(self.context[1])
-        elif self.conversing == False:
-            await client.send_message(message.channel, 'Not sure I got that, should I have done?')
-            self.conversing = True
-            self.context = ["feedback", msg]
+with open("token.txt") as o:
+    token = o.readline()
 
-    @client.event
-    async def on_ready():
-        print('Logged in as')
-        print(client.user.name)
-        print(client.user.id)
-        print('------')
-
-    @client.event
-    async def on_message(message):
-        bot.parse(message)
-
-bot = mr_bot()
+bot.run(token)
